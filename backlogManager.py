4ffbcd44a2c877e22dc2b2bcb8c13ebbecd0d217
Melissa Campbell <melissL77@hotmail.com>
2021-02-28 16:39:20 -0500

    functions toward top

--- backlogManager.py
+++ backlogManager.py
@@ -1,15 +1,12 @@
 from developer import Developer
 from task import Task
 from assignment import Assignment
-import csv # testing new stuff
-
-developer_list = [ Developer('Billy', 'Java'), Developer('Chris', '.NET')]
+import csv
 
 
-def developer_add_to_list(name, skill):
+def developer_add_to_list(name, skill, list):
     name = Developer(name, skill)
-    developer_list.append(name)
-
+    list.append(name)
 
 def developer_add_to_csv(developer_list):
     with open('developer.csv', 'w', newline="") as csvfile:
@@ -23,33 +20,36 @@ def developer_add_to_csv(developer_list):
 def developer_select_from_csv(skill):        
     with open('developer.csv') as csvfile:
         devreader = csv.DictReader(csvfile)
+        # list to store subset of csv, matching skill criteria
+        developer_list_by_skill = []
         for row in devreader:
-            # if row['skill'] == 'Java':
+            # if skillset matches skill parameter, add to list
             if row['skill'] == skill:
                 print(row['name'], row['skill'])
-            # print(row)
+                developer_add_to_list(row['name'], row['skill'], developer_list_by_skill)
+        return developer_list_by_skill
 
 
-developer_add_to_list('Tina', 'COBOL')
-developer_add_to_list('Sam', 'Java')
-developer_add_to_list('Sheila', '.NET')
-developer_add_to_list('Matt', 'COBOL')
-developer_add_to_list('Mike', 'Java')
-developer_add_to_list('Sally', '.NET')
-
-backlog = []
-
-
-def task_add(fix_or_enhancement, description, priority, estimate, status):
+def backlog_add_task(fix_or_enhancement, description, priority, estimate, status):
     backlog_item = Task(fix_or_enhancement, description, priority, estimate, status)
     backlog.append(backlog_item)
 
 
-task_add('Bug Fix', 'Fix what Tom messed up', 'Medium', 50, 'TODO')
-task_add('Bug Fix', 'Everything is on fire', 'Critical', 50, 'TODO')
-task_add('Bug Fix', 'System is Down', 'Critical', 40, 'TODO')
-task_add('Enhancement','Need new screen', 'High', 100, 'TODO' )
+developer_list = [ Developer('Billy', 'Java'), Developer('Chris', '.NET')]
 
+developer_add_to_list('Tina', 'COBOL', developer_list)
+developer_add_to_list('Sam', 'Java', developer_list)
+developer_add_to_list('Sheila', '.NET', developer_list)
+developer_add_to_list('Matt', 'COBOL', developer_list)
+developer_add_to_list('Mike', 'Java', developer_list)
+developer_add_to_list('Sally', '.NET', developer_list)
+
+backlog = []
+
+backlog_add_task('Bug Fix', 'Fix what Tom messed up', 'Medium', 50, 'TODO')
+backlog_add_task('Bug Fix', 'Everything is on fire', 'Critical', 50, 'TODO')
+backlog_add_task('Bug Fix', 'System is Down', 'Critical', 40, 'TODO')
+backlog_add_task('Enhancement','Need new screen', 'High', 100, 'TODO' )
 
 assignment_xref = []
 
@@ -64,7 +64,10 @@ for item in backlog:
 
 
 developer_add_to_csv(developer_list)
-developer_select_from_csv('Java')
+developer_list_by_skill = developer_select_from_csv('Java')
+
+for dev_skill in developer_list_by_skill:
+    print(f'Name: {dev_skill.name}, Skill: {dev_skill.skill}')
 
 for dev in developer_list:
     print(f'Name: {dev.name}, Allocation: {dev.allocation}')
